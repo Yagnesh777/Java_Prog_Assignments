62a2466997370f4afdb768afb85aebb1bfd0569e
Yagnesh777 <100506066+Yagnesh777@users.noreply.github.com>
2022-04-22 09:43:37 +0530

    Update and rename assign2C.java to profit_loss.java

--- profit_loss.java
+++ profit_loss.java
@@ -1,17 +1,17 @@
 import java.util.*;  
-class assign2C 
+class profit_loss
 {
 	public static void main(String args[]) 
 	{
-		int sp, cp, profit;
+		int sp, cp, res;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Selling Price for an item: ");
 		sp = sc.nextInt();
 		System.out.println("Enter Cost Price of an item: ");
 		cp = sc.nextInt();
 		
-		//Profit
-		profit = sp-cp;    
-		System.out.println("Profit for the item is "+profit);
+		// Selling price must be greater than cost price
+		res = sp-cp;    
+		System.out.println("Profit for the item is "+res);
 	}
 }
